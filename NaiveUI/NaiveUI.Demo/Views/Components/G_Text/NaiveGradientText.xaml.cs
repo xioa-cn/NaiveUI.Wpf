c8388371147ee0ca6999449a2f9770cfbf1ee0da
XIOA <1327916255@qq.com>
2025-05-12 15:16:41 +0800

    GradientTextAttach

---
+++ NaiveUI/NaiveUI.Demo/Views/Components/G_Text/NaiveGradientText.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace NaiveUI.Demo.Views.Components.G_Text;
+
+public partial class NaiveGradientText : UserControl
+{
+    public NaiveGradientText()
+    {
+        InitializeComponent();
+    }
+}
--- NaiveUI/NaiveUI.NControls/Attach/GradientTextAttach.cs
+++ NaiveUI/NaiveUI.NControls/Attach/GradientTextAttach.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace NaiveUI.NControls.Attach;
 
@@ -44,6 +45,74 @@ public class GradientTextAttach
                 break;
         }
 
+        var brsuh = d.GetValue(GradientTextAttach.GradientTextBrushProperty);
+        if(brsuh is null)
+        {
+            SetBrush(d, DefaultBrush());
+        }
+    }
 
+
+
+    public static Brush? GetGradientTextBrush(DependencyObject obj)
+    {
+        return (Brush?)obj.GetValue(GradientTextBrushProperty);
+    }
+
+    public static void SetGradientTextBrush(DependencyObject obj, Brush value)
+    {
+        obj.SetValue(GradientTextBrushProperty, value);
+    }
+
+    // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty GradientTextBrushProperty =
+       ElementBase.PropertyAttach<GradientTextAttach, Brush?>(nameof(GradientTextBrushProperty), null, GradientTextBrushChangedCallBack);
+
+    private static void GradientTextBrushChangedCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.NewValue is not Brush newBrush) return;
+
+        SetBrush(d, newBrush);
+    }
+
+    private static void SetBrush(DependencyObject d, Brush newBrush)
+    {
+        if (newBrush is null) newBrush = DefaultBrush();
+        switch (d)
+        {
+            case TextBlock textBlock:
+                textBlock.Foreground = newBrush;
+                break;
+            case TextBox textBox:
+                textBox.Foreground = newBrush;
+                break;
+            case TextElement textElement:
+                textElement.Foreground = newBrush;
+                break;
+            case Label label:
+                label.Foreground = newBrush;
+                break;
+            case AccessText access:
+                access.Foreground = newBrush;
+                break;
+            default:
+                throw new ArgumentException(nameof(d));
+        }
+    }
+
+    private static Brush DefaultBrush()
+    {
+        var brush = new LinearGradientBrush
+        {
+            StartPoint = new Point(0, 0),
+            EndPoint = new Point(1, 0),
+            GradientStops = new GradientStopCollection
+            {
+                new GradientStop(Colors.Blue, 0.0),
+                new GradientStop(Colors.Red, 1.0)
+            }
+        };
+
+        return brush;
     }
 }
