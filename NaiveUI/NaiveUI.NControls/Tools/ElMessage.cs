22371a2083d2fa03a7a24db64d8b23502763b1de
XIOA <1327916255@qq.com>
2025-07-04 15:54:17 +0800

    ElMessage

---
+++ NaiveUI/NaiveUI.NControls/Tools/ElMessage.cs
@@ -0,0 +1,24 @@
+﻿namespace NaiveUI.NControls.Tools;
+
+public static class NElMessage {
+    public static void Show(string message, string type = "info") {
+        switch (type.ToLower())
+        {
+            case "info":
+                ElMessage.Wpf.Utils.ElMessage.Info(message);
+                break;
+            case "success":
+                ElMessage.Wpf.Utils.ElMessage.Success(message);
+                break;
+            case "warning":
+                ElMessage.Wpf.Utils.ElMessage.Warning(message);
+                break;
+            case "error":
+                ElMessage.Wpf.Utils.ElMessage.Error(message);
+                break;
+            default:
+                ElMessage.Wpf.Utils.ElMessage.Info(message);
+                break;
+        }
+    }
+}
